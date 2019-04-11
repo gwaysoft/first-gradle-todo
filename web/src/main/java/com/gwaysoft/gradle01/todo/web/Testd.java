5f3bef94566a91b24e151dc7b7858387e315ccce
gwaysoft001 <gwaysoft@outlook.com>
2019-04-11 14:47:30 +0800

    test

--- web/src/main/java/com/gwaysoft/gradle01/todo/web/Testd.java
+++ web/src/main/java/com/gwaysoft/gradle01/todo/web/Testd.java
@@ -3,6 +3,6 @@ package com.gwaysoft.gradle01.todo.web;
 public class Testd {
     public static void main(String[] args) {
         String[] s ="dir/makeDir/ddd.txt".split("/\\w+.\\w+$");
-        System.out.println(s[0]);
+        System.out.println(s[0] + " Great");
     }
 }
